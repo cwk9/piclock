84482e642e7d7c0089afd7bbee365ad4675f1dd0
cwk9 <kilgourcolin@gmail.com>
2016-06-06 22:30:18 -0600

    Rebuild clock. Faster, Better, Stronger

--- piclock.py
+++ piclock.py
@@ -90,7 +90,7 @@ if ispi == 'yes':
 
 #Load alarm wave sound
 alarmwav = AudioSegment.from_wav(config['ALARMWAVS']['0'])
-alarmwavs = config.items('ALARMWAVS')
+alarmwavs = config.items('ALARMWAVS')[1]
 for row in alarmwavs:
     del row[0]
 print (alarmwavs)
